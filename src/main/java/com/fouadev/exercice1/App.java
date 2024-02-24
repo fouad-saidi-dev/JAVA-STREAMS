e772827b2aa428882e4e2d2cac25614fe717036e
FOUAD SAIDI <fouadsai17@gmail.com>
2024-02-24 22:39:52 +0100

    v1

--- src/main/java/com/fouadev/exercice1/App.java
+++ src/main/java/com/fouadev/exercice1/App.java
@@ -1,2 +1,64 @@
-package com.fouadev.exercice1;public class App {
+package com.fouadev.exercice1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        List<String> mots = new ArrayList<>();
+
+        mots.add("php");
+        mots.add("angular");
+        mots.add("java");
+        mots.add("react");
+
+        // 1 - filter
+        System.out.println("-----------Q1-----------");
+        mots.stream()
+                .filter((l) -> l.contains("a"))
+                .forEach(System.out::println);
+
+        // 2 - filter
+        System.out.println("------Q2--------");
+        mots.stream()
+                .filter((m) -> m.trim().length() > 3 )
+                        .map((c) -> new StringBuilder(c).reverse())
+                                .forEach(System.out::println);
+
+
+        // 3 - filter chaine contient "e" et aplatir chaque chaine en liste de caracteres
+        System.out.println("------Q3--------");
+        mots.stream()
+                .filter(m -> m.contains("e"))
+                .flatMap(m -> m.chars().mapToObj(c -> (char)c))
+                .toList()
+                .forEach(System.out::println);
+
+
+        // 4 - transformer en version majuscule
+        System.out.println("------Q4--------");
+        mots.stream()
+                .map(String::toUpperCase)
+                .forEach(System.out::println);
+
+        // 5 - Convertir chaque chaine en sa longuer
+        System.out.println("----------Q5----------");
+        mots.stream()
+                .map(String::length)
+                .forEach(System.out::println);
+
+        // 6 -
+        System.out.println("-----Q6-------");
+        mots.stream()
+                .flatMap(m -> m.chars().mapToObj(c -> (char)c))
+                .toList()
+                .forEach(System.out::println);
+
+        // 7 -
+        System.out.println("----------Q7----------");
+        mots.stream()
+                .map(c  -> c+"-"+mots.indexOf(c))
+                .forEach(System.out::println);
+
+    }
 }
--- src/main/java/com/fouadev/exercice2/App.java
+++ src/main/java/com/fouadev/exercice2/App.java
@@ -1,2 +1,61 @@
-package com.fouadev.exercice2;public class App {
+package com.fouadev.exercice2;
+
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) {
+
+        List<Employe> employes = new ArrayList<>();
+
+        employes.add(new Employe("ahmed","IT",9000));
+        employes.add(new Employe("mehdi","Back Office",6000));
+        employes.add(new Employe("rita","RH",7000));
+        employes.add(new Employe("brahim","PR",8000));
+
+
+        System.out.println("--------Q1----------");
+        double sum = employes.stream()
+                .mapToDouble(Employe::getSalaire)
+                .sum();
+
+        System.out.println(sum);
+
+        System.out.println("--------Q2----------");
+        employes.stream()
+                .map(Employe::getNom)
+                .sorted()
+                .forEach(System.out::println);
+
+        System.out.println("--------Q3----------");
+
+        Optional<Employe> minSalary = employes.stream()
+                .min((s1, s2) -> Double.compare(s1.getSalaire(), s2.getSalaire()));
+
+        System.out.println(minSalary.get());
+
+        System.out.println("--------Q4----------");
+
+        employes.stream()
+                .filter(employe -> employe.getSalaire() > 7000)
+                .forEach(System.out::println);
+
+        System.out.println("--------Q5----------");
+
+        Optional<Double> maxSalary = employes.stream()
+                .map(Employe::getSalaire)
+                .reduce(Double::max);
+
+        System.out.println(maxSalary);
+
+        System.out.println("--------Q6----------");
+        String concatNames = employes.stream()
+                .map(Employe::getNom)
+                .reduce("",(nom1,nom2) -> nom1+", "+nom2)
+                ;
+
+        System.out.println(concatNames);
+
+
+
+    }
 }
